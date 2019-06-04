412484fce42663e797c837e3b6b3a357b362f609
Alec Dusheck <alec@simplyalec.com>
2019-06-03 22:33:20 -0500

    Redo model

--- server.js
+++ server.js
@@ -2,52 +2,77 @@ const socks = require('socksv5');
 const Client = require('ssh2').Client;
 const config = require('./config');
 
-socks.createServer(function (info, accept, deny) {
-    let conn;
-    const makeSSH = () => {
-        if (conn) conn.removeAllListeners();
+let socksServer;
+let sshConnection;
 
-        conn = new Client();
-        conn.on('ready', function () {
-            conn.forwardOut(info.srcAddr,
-                info.srcPort,
-                info.dstAddr,
-                info.dstPort,
-                function (err, stream) {
-                    if (err) {
-                        conn.end();
-                        return deny();
-                    }
+const connect = async () => {
+    unbind(); // Unbind previous connections
+    sshConnection = new Client();
 
-                    let clientSocket;
-                    if (clientSocket = accept(true)) {
-                        stream.pipe(clientSocket).pipe(stream).on('close', function () {
-                            conn.end();
-                        })
-                    } else
-                        conn.end();
-                });
-        }).on('error', function () {
-            console.log("Failed to make socks proxy locally. It is already running?");
-            deny();
-        }).connect({
-            password: config.password,
-            username: config.username,
-            port: 22,
-            host: config.host
+    sshConnection.on("error", err => {
+        console.log("Got error in SSH connection:", err);
+        process.exit(1);
+    });
+
+    sshConnection.connect({
+        password: config.password,
+        username: config.username,
+        port: 22,
+        host: config.host,
+        keepAlive: true
+    });
+
+    await new Promise(resolve => {
+        sshConnection.on("ready", () => {
+            return resolve();
+        })
+    });
+
+    console.log("SSH connection prepped!");
+
+    socksServer = socks.createServer(socksHandler);
+    socksServer.listen(config.localPort, 'localhost', () => {
+        console.log('Socks5 server started on ' + config.localPort);
+    }).useAuth(socks.auth.None());
+
+    socksServer.on("error", err => {
+        console.log("Got error in socks5 connection:", err);
+    });
+};
+
+const socksHandler = async (info, accept, deny) => {
+    sshConnection.forwardOut(info.srcAddr,
+        info.srcPort,
+        info.dstAddr,
+        info.dstPort,
+        function (err, stream) {
+            if (err) {
+                sshConnection.end();
+                return deny();
+            }
+
+            let clientSocket;
+            if (clientSocket = accept(true)) {
+                stream.pipe(clientSocket).pipe(stream).on('close', function () {
+                    sshConnection.end();
+                }).on("error", err => {
+                    console.log("Testing error:", err);
+                })
+            } else
+                sshConnection.end();
         });
+};
 
-        conn.on("error", err => {
-            console.log("Caught error:", err);
+const unbind = () => {
+    if (socksServer) {
+        socksServer.removeAllListeners();
+        socksServer = undefined;
+    }
 
-            setTimeout(() => {
-                console.log("Rebuilding connection...");
-                makeSSH();
-            }, 10000);
-        });
-    };
+    if (sshConnection) {
+        sshConnection.removeAllListeners();
+        sshConnection = undefined;
+    }
+};
 
-    makeSSH();
-}).listen(config.localPort, 'localhost', function () {
-    console.log('Socks5 server started on ' + config.localPort);
-}).useAuth(socks.auth.None());
+connect();
